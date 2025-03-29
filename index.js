c88f26c60524e113a6b7463c3156305e082e77a0
Meet kothari <meetkothari07@gmail.com>
2025-03-29 16:07:42 +0530

    FE

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://whisper-room-ji640dt5e-mskothari07s-projects.vercel.app/', 
+    origin: '*', 
     methods: ['GET', 'POST']
   }
 });
